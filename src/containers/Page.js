535f93141f79fd5e1093029d16bc713a08588acf
Eduardo <drdpedroso@gmail.com>
2018-08-16 08:19:55 -0300

    Create function to add items to lists

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -2,10 +2,18 @@ import React from 'react'
 import {Row, Wrapper, ColumnLeft, Column, Title, RowRight} from '../style'
 import CheckList from '../components/CheckListItem'
 
-const data = [
-  {id: 1, title: 'Eat more', done: false},
-  {id: 2, title: 'Eat mor2', done: true}
-]
+class AddItemInput extends React.PureComponent {
+
+  state = {
+    text: ''
+  }
+
+  render(){
+    return (<div><button onClick={() => this.props.addItem(this.state.text, this.props.column)}>Add</button>
+      <input onChange={({target}) => {this.setState({text: target.value})}} type="text" value={this.state.text}/>
+        </div>)
+  }
+}
 
 class Page extends React.PureComponent {
   constructor(){
@@ -24,14 +32,13 @@ class Page extends React.PureComponent {
     localStorage.setItem(list, dataToSave)
   }
 
-  addItem(title, column) {
-    const columnData = JSON.parse(localStorage.getItem('scheduled')) || []
-    debugger
+  addItem = (title, column) => {
+    const columnData = JSON.parse(localStorage.getItem(column)) || []
     const id = columnData.length + 1
     columnData.push({id, title, done: false})
     localStorage.setItem(column, JSON.stringify(columnData))
     this.setState({
-      scheduled: columnData
+      [column]: columnData
     })
   }
 
@@ -46,20 +53,30 @@ class Page extends React.PureComponent {
               <Column>
                 <Title>Scheduled</Title>
                 <Row style={{maxHeight: 40}}>
-                  <button onClick={() => this.addItem('title', 'scheduled')}>Add</button><input type="text"/>
+                  <AddItemInput addItem={this.addItem} column="scheduled" />
                 </Row>
                 <CheckList data={scheduled} onChangeItem={data => this.onChangeItem(data, 'scheduled')}/>
               </Column>
             </RowRight>
             <Row>
-              <Title>To Do</Title>
-              <CheckList data={todo} onChangeItem={data => this.onChangeItem(data, 'todo')}/>
+              <Column>
+                <Title>To Do</Title>
+                <Row style={{maxHeight: 40}}>
+                  <AddItemInput addItem={this.addItem} column="todo" />
+                </Row>
+                <CheckList data={todo} onChangeItem={data => this.onChangeItem(data, 'todo')}/>
+              </Column>
             </Row>
           </ColumnLeft>
           <Column>
             <RowRight>
-              <Title>Goals</Title>
-              <CheckList data={goals} onChangeItem={data => this.onChangeItem(data, 'goals')}/>
+              <Column>
+                <Title>Goals</Title>
+                <Row style={{maxHeight: 40}}>
+                  <AddItemInput addItem={this.addItem} column="goals" />
+                </Row>
+                <CheckList data={goals} onChangeItem={data => this.onChangeItem(data, 'goals')}/>
+              </Column>
             </RowRight>
             <Row>
               <Title>Motivation</Title>
